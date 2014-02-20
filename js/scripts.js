8dc024c8286137b672cad949a25c639a63b6027c
Blackbaud-MitchellDunaway <mitchell.dunaway@blackbaud.com>
2014-02-20 17:02:42 -0600

    Feb 20, 2014

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 Y.use('jquery-noconflict', function() {	
 	jQuery(document).ready(function($){
 
+
+		/* temporary element toggles
+		------------------------------------------------------------ */
+		$('#login_logged_in').hide();
+
 		$('#user_session').change(function(){
 			if($(this).val() === "1") {
 				$('#lLogonForm').hide();
+				$('#login_logged_in').show();
 			} else {
 				$('#lLogonForm').show();
+				$('#login_logged_in').hide();
 			}
 		})
 
@@ -16,6 +23,61 @@ Y.use('jquery-noconflict', function() {
 				$('#sidebar').hide();
 			}
 		})
+
+
+		/* 	placeholder text
+			on focus: if the value matches the title, clear it
+			on blur: if the value is empty, populate with the title
+		------------------------------------------------------------ */
+		$("input[type=text], input[type=password]").focusin(function(){  
+			var title = $(this).attr("title");  
+			if (title == $(this).val()) {  
+				$(this).val("");  
+			}  
+		});  
+		$("input[type=text], input[type=password]").focusout(function(){  
+			var title = $(this).attr("title"); 
+			if ($(this).val() == "") {  
+				$(this).val(title);  
+			}  
+		});
+
+
+		/* 	panel show/hide
+			when the hamburger button is clicked
+				if not active
+					add active state
+					add click handler to body to close panel
+				if active
+					remove active state
+					remove body click handler
+		------------------------------------------------------------ */
+
+		$('#nav_reveal, #nav_reveal::after').click(function(e){
+			e.preventDefault();
+			var elements = $('#header_utilities, #nav_reveal');
+			if (elements.hasClass('active')) {
+				panel_hide(elements);
+			} else {
+				panel_show(elements);
+			}
+		})
+
+		function panel_show(elements){
+			elements.addClass('active');
+			$('body').bind('click touchstart',function(e){
+				if (!$(e.target).closest(elements).length) {
+					panel_hide(elements);
+				}
+			});
+		}
+
+		function panel_hide(elements){
+			elements.removeClass('active');
+			$('body').unbind('click touchstart');
+		}
+
+
 
     }); //end jQuery(document).ready()
 }); //end Y.use('jquery-noconflict')
--- notes.md
+++ notes.md
@@ -1,5 +1,5 @@
 TO DO
 
-1. strip out bad code
-2. add in good code from template
-3. revise
+
+
+
